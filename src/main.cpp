ea83e78ef4042638467d5a4403840fe611e523a0
Zedrik Cayne <zedrikcayne@gmail.com>
2024-11-29 12:40:01 -0500

    Adding a post route, and deleting the stuff I put in for testing exact and prefix routes.

--- src/main.cpp
+++ src/main.cpp
@@ -143,12 +143,10 @@ bool doQuit( struct CS_ClientInfo *info ) {
 }
 
 struct CS_Route serverRoutes[] = {
-    { CS_HTTP_METHOD_GET,  CS_ROUTE_TYPE_PREFIX, 0, "/Prefix/Match/Me", CS_Diagnostic200 },
-    { CS_HTTP_METHOD_GET,  CS_ROUTE_TYPE_EXACT, 0, "/Very/Match/Me", CS_Diagnostic200 },
     { CS_HTTP_METHOD_GET,  CS_ROUTE_TYPE_EXACT, 0, "/googlelogin", CS_Diagnostic200 },
     { CS_HTTP_METHOD_POST, CS_ROUTE_TYPE_EXACT, 0, "/googlelogin", CS_Diagnostic200 },
     { CS_HTTP_METHOD_GET,  CS_ROUTE_TYPE_PREFIX, 0, "/api", fudge },
-    { CS_HTTP_METHOD_GET,  CS_ROUTE_TYPE_EXACT, 0, "/quit", doQuit },
+    { CS_HTTP_METHOD_POST, CS_ROUTE_TYPE_PREFIX, 0, "/api", fudge },
     { CS_HTTP_METHOD_HEAD, CS_ROUTE_TYPE_WILDCARD, 0, "", CS_FileServer },
     { CS_HTTP_METHOD_GET,  CS_ROUTE_TYPE_WILDCARD, 0, "", CS_FileServer },
 };
