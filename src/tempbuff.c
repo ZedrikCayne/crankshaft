8ea76f9641d46286860f94d28893787609eb4879
Zedrik Cayne <zedrikcayne@gmail.com>
2025-06-28 20:48:26 -0400

    Fixing a deadlock on temp buffer cycle. Amazed we've not seen this yet.

--- src/tempbuff.c
+++ src/tempbuff.c
@@ -61,8 +61,7 @@ static void *privateAllocate( struct CS_TempBuffer *buffer, int size, int align 
     pthread_mutex_lock(&buffer->storageMutex);
     if( buffer->current + realSize > buffer->end ) {
         CS_LOG_TRACE("Temp Buff %s cycled", buffer->name);
-        buffer->current = buffer->buffer + realSize;
-        return buffer->buffer;
+        buffer->current = buffer->buffer;
     }
     void *returnValue = buffer->current;
     buffer->current += realSize;
